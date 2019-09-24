3b0632551ebbfb8e3a7a8a8fec74b0698a23d00d
Plasnid <stuart@sackler.ca>
2019-09-24 12:09:26 -0400

    simplified the pathing for the dev server content base

--- src/tamo.js
+++ src/tamo.js
@@ -1,6 +1,6 @@
 export default class Tamo{
     constructor(){
         console.log("I don't like food anymore!");
-        console.log("okay maybe I do!!!!???");
+        console.log("okay maybe I do!!!!??? Ooooh yeah!");
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     //output, where our files compile to
     output: {
-        path: path.resolve(__dirname, './dist/'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
     devServer:{
